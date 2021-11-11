c60ee776ebbecf8b5043dbf348def54b7ad6d1e2
Andrei <freemail_2019@mail.ru>
2021-11-11 14:47:24 +0800

    Начало2

--- shop/digits/models.py
+++ shop/digits/models.py
@@ -29,6 +29,33 @@ class Product(models.Model):
         return self.title
 
 
+class Notebook(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name=' Тип дисплея')
+    processor_frec = models.CharField(max_length=255, verbose_name='Частота процессора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    video = models.CharField(max_length=255, verbose_name='Видеокарта')
+    time_without_charge = models.CharField(max_length=255, verbose_name='Время работы аакумулятора')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category.name, self.title)
+
+
+class Smartphone(Product):
+    diagonal = models.CharField(max_length=255, verbose_name='Диагональ')
+    display_type = models.CharField(max_length=255, verbose_name=' Тип дисплея')
+    resolution = models.CharField(max_length=255, verbose_name='Разрешение экрана')
+    accum_volume = models.CharField(max_length=255, verbose_name='Емкость аккумулятора')
+    ram = models.CharField(max_length=255, verbose_name='Оперативная память')
+    sd = models.BooleanField(default=True, verbose_name='Поддержка sd карты')
+    sd_volume = models.CharField(max_length=255, verbose_name='Максимальный объем sd карты')
+    main_cam_mp = models.CharField(max_length=255, verbose_name='Основная камера')
+    frontal_cam_mp = models.CharField(max_length=255, verbose_name='Фронтальная камера')
+
+    def __str__(self):
+        return '{} : {}'.format(self.category.name, self.title)
+
+
 class CartProduct(models.Model):
     user = models.ForeignKey('Customer', verbose_name='покупатель', on_delete=models.CASCADE)
     cart = models.ForeignKey('Cart', verbose_name='карзина', on_delete=models.CASCADE, related_name='relates_products')
